8fdffa4613b5d3a7fb26f1aa61dab76ec2b460f9
Giffari17 <gyusrulkhoi@gmail.com>
2023-03-09 07:50:26 +0700

    Init Project

---
+++ src/views/HomePage.jsx
@@ -0,0 +1,13 @@
+export function HomePage() {
+    return (
+        <div>
+            <h1>Posisi yang dilamar NodeJS</h1>
+            <h1>Disuruh Ngerjain C#</h1>
+            <h1>Disuruh Ngerjain Jadinya Ga Sempatlah wkwkw</h1>
+            <h1>C# nya selesai Reactnya blm jadi</h1>
+            
+            
+            <h1>Kalo penguji suruh pake NodeJS Dari Kemarin Udah Lama Selesai</h1>
+        </div>
+    )
+}
